3f7e4b926fa09fd835285498d649639e3efe87f2
connorjcantrell <connorjcantrell@gmail.com>
2022-01-27 10:51:41 -0800

    format using black

--- ticket_smart_contract.py
+++ ticket_smart_contract.py
@@ -3,61 +3,51 @@ from pyteal import *
 
 def approval_program():
 
+    on_create_sale_begin = (Btoi(Txn.application_args[0]),)
+    on_create_sale_end = (Btoi(Txn.application_args[1]),)
+    on_create_event_start = (Btoi(Txn.application_args[2]),)
+    on_create_price = (Btoi(Txn.application_args[3]),)
 
     on_create = Seq(
-        on_create_sale_begin = Btio(Txn.application_args[0]),
-        on_create_sale_end = Btoi(Txn.application_args[1]),
-        on_create_event_start = Btoi(Txn.application_args[2]),
-        on_create_price = Btoi(Txn.application_args[3]),
         [
-            App.globalPut( Bytes("Creator"), Txn.sender() ),
-            App.globalPut( Bytes("Sale Begin"), on_create_sale_begin ),
-            App.globalPut( Bytes("Sale End"), on_create_sale_end ),
-            App.globalPut( Bytes("Event Start"), on_create_event_start ),
-            App.globalPut( Bytes("Price"), on_create_price ),
-            App.globalPut( Bytes("Event Name"), Txn.application_args[4] ),
-            App.globalPut( Bytes("Location"), Txn.application_args[5] ),
-            App.globalPut( Bytes("Amount"), Txn.application_args[6] ),
+            App.globalPut(Bytes("Creator"), Txn.sender()),
+            App.globalPut(Bytes("Sale Begin"), on_create_sale_begin),
+            App.globalPut(Bytes("Sale End"), on_create_sale_end),
+            App.globalPut(Bytes("Event Start"), on_create_event_start),
+            App.globalPut(Bytes("Price"), on_create_price),
+            App.globalPut(Bytes("Event Name"), Txn.application_args[4]),
+            App.globalPut(Bytes("Location"), Txn.application_args[5]),
+            App.globalPut(Bytes("Amount"), Txn.application_args[6]),
             Assert(
                 And(
                     Global.latest_timestamp() < on_create_sale_begin,
                     on_create_sale_begin < on_create_sale_end,
-                    on_create_sale_begin < on_create_event_start
+                    on_create_sale_begin < on_create_event_start,
                 ),
             ),
-            Return( Int(1) ),
+            Return(Int(1)),
         ]
     )
 
-    is_creator = Txn.sender() == App.globalGet( Bytes("Creator") )
-
+    is_creator = Txn.sender() == App.globalGet(Bytes("Creator"))
 
     delete_application = Seq(
-
         [
-            Assert(is_creator == Int(1) ),
+            Assert(is_creator == Int(1)),
             Return(Int(1)),
         ]
     )
 
     update_application = Seq(
-
         [
-            Assert(is_creator == Int(1) ),
+            Assert(is_creator == Int(1)),
             Return(Int(1)),
         ]
     )
 
-    on_closeout = Seq(
+    on_closeout = Seq(Return(Int(1)))
 
-        Return(Int(1))
-    )
-
-    on_opt_in = Seq(
-
-        Return(Int(1))
-    )
-
+    on_opt_in = Seq(Return(Int(1)))
 
     program = Cond(
         [Txn.application_id() == Int(0), on_create],
