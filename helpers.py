ad8dddfefd46ba6669ad5fabb3e6800f35c5f452
connorjcantrell <connorjcantrell@gmail.com>
2022-01-27 13:36:26 -0800

    add deploy and helper modules

---
+++ helpers.py
@@ -0,0 +1,280 @@
+import base64
+
+from algosdk.future import transaction
+from algosdk import account, mnemonic
+
+
+# helper function to compile program source
+def compile_program(client, source_code):
+    compile_response = client.compile(source_code)
+    return base64.b64decode(compile_response["result"])
+
+
+# helper function that converts a mnemonic passphrase into a private signing key
+def get_private_key_from_mnemonic(mn):
+    private_key = mnemonic.to_private_key(mn)
+    return private_key
+
+
+# helper function that waits for a given txid to be confirmed by the network
+def wait_for_confirmation(client, txid):
+    last_round = client.status().get("last-round")
+    txinfo = client.pending_transaction_info(txid)
+    while not (txinfo.get("confirmed-round") and txinfo.get("confirmed-round") > 0):
+        print("Waiting for confirmation...")
+        last_round += 1
+        client.status_after_block(last_round)
+        txinfo = client.pending_transaction_info(txid)
+    print(
+        "Transaction {} confirmed in round {}.".format(
+            txid, txinfo.get("confirmed-round")
+        )
+    )
+    return txinfo
+
+
+def wait_for_round(client, round):
+    last_round = client.status().get("last-round")
+    print(f"Waiting for round {round}")
+    while last_round < round:
+        last_round += 1
+        client.status_after_block(last_round)
+        print(f"Round {last_round}")
+
+
+# create new application
+def create_app(
+    client,
+    private_key,
+    approval_program,
+    clear_program,
+    global_schema,
+    local_schema,
+    app_args,
+):
+    # define sender as creator
+    sender = account.address_from_private_key(private_key)
+
+    # declare on_complete as NoOp
+    on_complete = transaction.OnComplete.NoOpOC.real
+
+    # get node suggested parameters
+    params = client.suggested_params()
+
+
+    # create unsigned transaction
+    txn = transaction.ApplicationCreateTxn(
+        sender,
+        params,
+        on_complete,
+        approval_program,
+        clear_program,
+        global_schema,
+        local_schema,
+        app_args
+    )
+
+    # sign transaction
+    signed_txn = txn.sign(private_key)
+    tx_id = signed_txn.transaction.get_txid()
+
+    # send transaction
+    client.send_transactions([signed_txn])
+
+    # await confirmation
+    wait_for_confirmation(client, tx_id)
+
+    # display results
+    transaction_response = client.pending_transaction_info(tx_id)
+    app_id = transaction_response["application-index"]
+    print("Created new app-id:", app_id)
+
+    return app_id
+
+
+# opt-in to application
+def opt_in_app(client, private_key, index):
+    # declare sender
+    sender = account.address_from_private_key(private_key)
+    print("OptIn from account: ", sender)
+
+    # get node suggested parameters
+    params = client.suggested_params()
+    # comment out the next two (2) lines to use suggested fees
+    params.flat_fee = True
+    params.fee = 1000
+
+    # create unsigned transaction
+    txn = transaction.ApplicationOptInTxn(sender, params, index)
+
+    # sign transaction
+    signed_txn = txn.sign(private_key)
+    tx_id = signed_txn.transaction.get_txid()
+
+    # send transaction
+    client.send_transactions([signed_txn])
+
+    # await confirmation
+    wait_for_confirmation(client, tx_id)
+
+    # display results
+    transaction_response = client.pending_transaction_info(tx_id)
+    print("OptIn to app-id:", transaction_response["txn"]["txn"]["apid"])
+
+
+# call application
+def call_app(client, private_key, index, app_args):
+    # declare sender
+    sender = account.address_from_private_key(private_key)
+    print("Call from account:", sender)
+
+    # get node suggested parameters
+    params = client.suggested_params()
+    # comment out the next two (2) lines to use suggested fees
+    params.flat_fee = True
+    params.fee = 1000
+
+    # create unsigned transaction
+    txn = transaction.ApplicationNoOpTxn(sender, params, index, app_args)
+
+    # sign transaction
+    signed_txn = txn.sign(private_key)
+    tx_id = signed_txn.transaction.get_txid()
+
+    # send transaction
+    client.send_transactions([signed_txn])
+
+    # await confirmation
+    wait_for_confirmation(client, tx_id)
+
+
+def format_state(state):
+    formatted = {}
+    for item in state:
+        key = item["key"]
+        value = item["value"]
+        formatted_key = base64.b64decode(key).decode("utf-8")
+        if value["type"] == 1:
+            # byte string
+            if formatted_key == "voted":
+                formatted_value = base64.b64decode(value["bytes"]).decode("utf-8")
+            else:
+                formatted_value = value["bytes"]
+            formatted[formatted_key] = formatted_value
+        else:
+            # integer
+            formatted[formatted_key] = value["uint"]
+    return formatted
+
+
+# read user local state
+def read_local_state(client, addr, app_id):
+    results = client.account_info(addr)
+    for local_state in results["apps-local-state"]:
+        if local_state["id"] == app_id:
+            if "key-value" not in local_state:
+                return {}
+            return format_state(local_state["key-value"])
+    return {}
+
+
+# read app global state
+def read_global_state(client, addr, app_id):
+    results = client.account_info(addr)
+    apps_created = results["created-apps"]
+    for app in apps_created:
+        if app["id"] == app_id:
+            return format_state(app["params"]["global-state"])
+    return {}
+
+
+# delete application
+def delete_app(client, private_key, index):
+    # declare sender
+    sender = account.address_from_private_key(private_key)
+
+    # get node suggested parameters
+    params = client.suggested_params()
+    # comment out the next two (2) lines to use suggested fees
+    params.flat_fee = True
+    params.fee = 1000
+
+    # create unsigned transaction
+    txn = transaction.ApplicationDeleteTxn(sender, params, index)
+
+    # sign transaction
+    signed_txn = txn.sign(private_key)
+    tx_id = signed_txn.transaction.get_txid()
+
+    # send transaction
+    client.send_transactions([signed_txn])
+
+    # await confirmation
+    wait_for_confirmation(client, tx_id)
+
+    # display results
+    transaction_response = client.pending_transaction_info(tx_id)
+    print("Deleted app-id:", transaction_response["txn"]["txn"]["apid"])
+
+
+# close out from application
+def close_out_app(client, private_key, index):
+    # declare sender
+    sender = account.address_from_private_key(private_key)
+
+    # get node suggested parameters
+    params = client.suggested_params()
+    # comment out the next two (2) lines to use suggested fees
+    params.flat_fee = True
+    params.fee = 1000
+
+    # create unsigned transaction
+    txn = transaction.ApplicationCloseOutTxn(sender, params, index)
+
+    # sign transaction
+    signed_txn = txn.sign(private_key)
+    tx_id = signed_txn.transaction.get_txid()
+
+    # send transaction
+    client.send_transactions([signed_txn])
+
+    # await confirmation
+    wait_for_confirmation(client, tx_id)
+
+    # display results
+    transaction_response = client.pending_transaction_info(tx_id)
+    print("Closed out from app-id: ", transaction_response["txn"]["txn"]["apid"])
+
+
+# clear application
+def clear_app(client, private_key, index):
+    # declare sender
+    sender = account.address_from_private_key(private_key)
+
+    # get node suggested parameters
+    params = client.suggested_params()
+    # comment out the next two (2) lines to use suggested fees
+    params.flat_fee = True
+    params.fee = 1000
+
+    # create unsigned transaction
+    txn = transaction.ApplicationClearStateTxn(sender, params, index)
+
+    # sign transaction
+    signed_txn = txn.sign(private_key)
+    tx_id = signed_txn.transaction.get_txid()
+
+    # send transaction
+    client.send_transactions([signed_txn])
+
+    # await confirmation
+    wait_for_confirmation(client, tx_id)
+
+    # display results
+    transaction_response = client.pending_transaction_info(tx_id)
+    print("Cleared app-id:", transaction_response["txn"]["txn"]["apid"])
+
+
+# convert 64 bit integer i to byte string
+def intToBytes(i):
+    return i.to_bytes(8, "big") 
