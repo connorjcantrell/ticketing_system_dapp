5ccd0c2ddf503341b241895fbb5d09272d4b8bc5
connorjcantrell <connorjcantrell@gmail.com>
2022-01-26 23:37:20 -0800

    initialized variables in approval_program

--- ticket_smart_contract.py
+++ ticket_smart_contract.py
@@ -4,10 +4,27 @@ from pyteal import *
 def approval_program():
 
 
-    on_creation = Seq(
+    on_create = Seq(
+        on_create_sale_begin = Btio(Txn.application_args[0]),
+        on_create_sale_end = Btoi(Txn.application_args[1]),
+        on_create_event_start = Btoi(Txn.application_args[2]),
+        on_create_price = Btoi(Txn.application_args[3]),
         [
             App.globalPut( Bytes("Creator"), Txn.sender() ),
-
+            App.globalPut( Bytes("Sale Begin"), on_create_sale_begin ),
+            App.globalPut( Bytes("Sale End"), on_create_sale_end ),
+            App.globalPut( Bytes("Event Start"), on_create_event_start ),
+            App.globalPut( Bytes("Price"), on_create_price ),
+            App.globalPut( Bytes("Event Name"), Txn.application_args[4] ),
+            App.globalPut( Bytes("Location"), Txn.application_args[5] ),
+            App.globalPut( Bytes("Amount"), Txn.application_args[6] ),
+            Assert(
+                And(
+                    Global.latest_timestamp() < on_create_sale_begin,
+                    on_create_sale_begin < on_create_sale_end,
+                    on_create_sale_begin < on_create_event_start
+                ),
+            ),
             Return( Int(1) ),
         ]
     )
@@ -43,7 +60,7 @@ def approval_program():
 
 
     program = Cond(
-        [Txn.application_id() == Int(0), on_creation],
+        [Txn.application_id() == Int(0), on_create],
         [Txn.on_completion() == OnComplete.DeleteApplication, delete_application],
         [Txn.on_completion() == OnComplete.UpdateApplication, update_application],
         [Txn.on_completion() == OnComplete.CloseOut, on_closeout],
